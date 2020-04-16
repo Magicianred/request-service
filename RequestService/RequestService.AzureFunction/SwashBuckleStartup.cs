6cb34f74631d1dfb159963722b710be95befc77c
Jawwad Mukhtar <jawwad.mukhtar@gmail.com>
2020-04-16 16:06:57 +0100

    Add swagger

---
+++ RequestService/RequestService.AzureFunction/SwashBuckleStartup.cs
@@ -0,0 +1,22 @@
+﻿using AzureFunctions.Extensions.Swashbuckle;
+using Microsoft.Azure.WebJobs;
+using Microsoft.Azure.WebJobs.Hosting;
+using RequestService.AzureFunction;
+using System;
+using System.Collections.Generic;
+using System.Reflection;
+using System.Text;
+
+[assembly: WebJobsStartup(typeof(SwashBuckleStartup))]
+namespace RequestService.AzureFunction
+{
+    internal class SwashBuckleStartup : IWebJobsStartup
+    {
+        public void Configure(IWebJobsBuilder builder)
+        {
+            //Register the extension
+            builder.AddSwashBuckle(Assembly.GetExecutingAssembly());
+
+        }
+    }
+}
