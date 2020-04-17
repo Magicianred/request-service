e589aa5e1f83950059c11f660448352fdc9e0813
mike-f50 <58040070+mike-f50@users.noreply.github.com>
2020-04-17 11:30:48 +0100

    Standardising on lowercase c in Postcode

--- RequestService/RequestService.Core/Domains/Entities/LogRequestRequest.cs
+++ RequestService/RequestService.Core/Domains/Entities/LogRequestRequest.cs
@@ -7,6 +7,6 @@ namespace RequestService.Core.Domains.Entities
 {
     public class LogRequestRequest : IRequest<LogRequestResponse>
     {
-        public string PostCode { get; set; }
+        public string Postcode { get; set; }
     }
 }
