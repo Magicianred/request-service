7f43c7b706ed5dc060bac8ad99350a11f3c12f83
Jawwad Mukhtar <jawwad@factor-50.co.uk>
2020-05-11 17:11:31 +0100

    add migrations

---
+++ RequestService/RequestService.Repo/ApplicationDbContextFactory.cs
@@ -0,0 +1,36 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Design;
+using Microsoft.Extensions.Configuration;
+using RequestService.Core.Config;
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Text;
+
+namespace RequestService.Repo
+{
+    public class ApplicationDbContextFactory : IDesignTimeDbContextFactory<ApplicationDbContext>
+    {
+
+        public ApplicationDbContext CreateDbContext(string[] args)
+        {
+            // get connection string from AddressService.AzureFunction" project to avoid duplication
+            string azureFunctionDirectory = Directory.GetCurrentDirectory().Replace("RequestService.Repo", "RequestService.AzureFunction");
+
+            IConfigurationRoot configuration = new ConfigurationBuilder()
+                .SetBasePath(azureFunctionDirectory)
+                .AddJsonFile("appsettings.json")
+                .Build();
+
+            var connectionStringSettings = configuration.GetSection("ConnectionStrings");
+            var connectionStrings = new ConnectionStrings();
+            connectionStringSettings.Bind(connectionStrings);
+
+            var optionsBuilder = new DbContextOptionsBuilder<ApplicationDbContext>();
+            optionsBuilder.UseSqlServer(connectionStrings.RequestService);
+
+            Console.WriteLine($"Using following connection string for Entity Framework: {connectionStrings.RequestService}");
+            return new ApplicationDbContext(optionsBuilder.Options);
+        }
+    }
+}
