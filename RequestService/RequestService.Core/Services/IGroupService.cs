70d1bfaf06f22411e26bc90a9eb0cb0a3b58f159
Jawwad Mukhtar <jawwad@factor-50.co.uk>
2020-07-03 11:59:32 +0100

    add link to group service

---
+++ RequestService/RequestService.Core/Services/IGroupService.cs
@@ -0,0 +1,13 @@
+﻿using HelpMyStreet.Contracts.GroupService.Request;
+using HelpMyStreet.Contracts.GroupService.Response;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace RequestService.Core.Services
+{
+    public interface IGroupService
+    {
+        Task<GetNewRequestActionsResponse> GetNewRequestActions(GetNewRequestActionsRequest request, CancellationToken cancellationToken);
+    }
+
+}
