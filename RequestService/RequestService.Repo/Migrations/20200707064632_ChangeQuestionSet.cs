d087cad8c836e1f6935ea3f993c6af48f8454383
Jawwad Mukhtar <jawwad@factor-50.co.uk>
2020-07-07 07:47:13 +0100

    FTLOS-54

---
+++ RequestService/RequestService.Repo/Migrations/20200707064632_ChangeQuestionSet.cs
@@ -0,0 +1,29 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace RequestService.Repo.Migrations
+{
+    public partial class ChangeQuestionSet : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.UpdateData(
+                schema: "QuestionSet",
+                table: "Question",
+                keyColumn: "ID",
+                keyValue: 4,
+                column: "AdditionalData",
+                value: "[{\"Key\":\"keyworkers\",\"Value\":\"Key workers\"},{\"Key\":\"somonekeyworkers\",\"Value\":\"Someone helping key workers stay safe in their role (e.g. care home residents, visitors etc.)\"},{\"Key\":\"memberspublic\",\"Value\":\"Members of the public\"}]");
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.UpdateData(
+                schema: "QuestionSet",
+                table: "Question",
+                keyColumn: "ID",
+                keyValue: 4,
+                column: "AdditionalData",
+                value: "[{\"Key\":\"keyworkers\",\"Value\":\"Key workers\"},{\"Key\":\"somonekeyworkers\",\"Value\":\"Someone helping key workers stay safe in their role (e.g. care home residents, visitors etc.)\"},{\"Key\":\"someone\",\"Value\":\"Someone else\"}]");
+        }
+    }
+}
