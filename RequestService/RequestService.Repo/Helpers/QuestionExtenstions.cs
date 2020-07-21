034c5693c518eeb5fdd3cfba5d7b77bdee574784
Jawwad Mukhtar <jawwad@factor-50.co.uk>
2020-07-21 10:48:17 +0100

    remove commented out code

--- RequestService/RequestService.Repo/Helpers/QuestionExtenstions.cs
+++ RequestService/RequestService.Repo/Helpers/QuestionExtenstions.cs
@@ -20,35 +20,30 @@ namespace RequestService.Repo.Helpers
                 Id = (int)Questions.SupportRequesting,
                 Name = "Please tell us more about the help or support you're requesting",
                 QuestionType = (int)QuestionType.MultiLineText,
-                //Required = false,
                 AdditionalData = GetAdditionalData(Questions.SupportRequesting)
             });
             entity.HasData(new Question {
                 Id = (int)Questions.FaceMask_SpecificRequirements,
                 Name = "Please tell us about any specific requirements (e.g. size, colour, style etc.)",
                 QuestionType = (int)QuestionType.MultiLineText,
-                //Required = false,
                 AdditionalData = GetAdditionalData(Questions.FaceMask_SpecificRequirements)
             });
             entity.HasData(new Question {
                 Id = (int)Questions.FaceMask_Amount,
                 Name = "How many face coverings do you need?",
                 QuestionType = (int)QuestionType.Number,
-                //Required = true,
                 AdditionalData = GetAdditionalData(Questions.FaceMask_Amount)
             });
             entity.HasData(new Question {
                 Id = (int)Questions.FaceMask_Recipient,
                 Name = "Who will be using the face coverings?",
                 QuestionType = (int)QuestionType.Radio,
-                //Required = false,
                 AdditionalData = GetAdditionalData(Questions.FaceMask_Recipient)
             });
             entity.HasData(new Question { 
              Id = (int)Questions.FaceMask_Cost,
                 Name = "Are you able to pay the cost of materials for your face covering(usually £2 - £3 each) ?",
                 QuestionType = (int)QuestionType.Radio,
-                //Required = false,
                 AdditionalData = GetAdditionalData(Questions.FaceMask_Cost)
             });
 
@@ -56,7 +51,6 @@ namespace RequestService.Repo.Helpers
             Id = (int)Questions.IsHealthCritical,
                 Name = "Is this request critical to someone's health or wellbeing?",
                 QuestionType = (int)QuestionType.Radio,
-                //Required = true,
                 AdditionalData = GetAdditionalData(Questions.IsHealthCritical) 
             });
             entity.HasData(new Question
@@ -64,7 +58,6 @@ namespace RequestService.Repo.Helpers
                 Id = (int)Questions.WillYouCompleteYourself,
                 Name = "Will you complete this request yourself?",
                 QuestionType = (int)QuestionType.Radio,
-                //Required = true,
                 AdditionalData = GetAdditionalData(Questions.WillYouCompleteYourself)
             });
         }
