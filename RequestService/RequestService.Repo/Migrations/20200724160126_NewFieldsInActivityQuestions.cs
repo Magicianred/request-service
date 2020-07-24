e6b1b53bca18843a07d06159da67f369b4821a05
Mike Matthews <mike@factor-50.co.uk>
2020-07-24 17:06:28 +0100

    Adding fields to ActivityQuestions (migration)

---
+++ RequestService/RequestService.Repo/Migrations/20200724160126_NewFieldsInActivityQuestions.cs
@@ -0,0 +1,173 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace RequestService.Repo.Migrations
+{
+    public partial class NewFieldsInActivityQuestions : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<string>(
+                name: "Location",
+                schema: "QuestionSet",
+                table: "ActivityQuestions",
+                nullable: true);
+
+            migrationBuilder.AddColumn<string>(
+                name: "PlaceholderText",
+                schema: "QuestionSet",
+                table: "ActivityQuestions",
+                nullable: true);
+
+            migrationBuilder.AddColumn<string>(
+                name: "Subtext",
+                schema: "QuestionSet",
+                table: "ActivityQuestions",
+                nullable: true);
+
+            migrationBuilder.InsertData(
+                schema: "QuestionSet",
+                table: "ActivityQuestions",
+                columns: new[] { "ActivityID", "QuestionID", "RequestFormVariantID", "Location", "Order", "PlaceholderText", "Required", "Subtext" },
+                values: new object[,]
+                {
+                    { 1, 1, 6, null, 1, null, false, null },
+                    { 12, 5, 6, null, 4, null, false, null },
+                    { 12, 4, 6, null, 3, null, false, null },
+                    { 12, 3, 6, null, 1, null, true, null },
+                    { 12, 2, 6, null, 2, null, false, null },
+                    { 11, 1, 6, null, 1, null, false, null },
+                    { 10, 1, 6, null, 1, null, false, null },
+                    { 13, 1, 6, null, 1, null, false, null },
+                    { 9, 1, 6, null, 1, null, false, null },
+                    { 7, 1, 6, null, 1, null, false, null },
+                    { 6, 1, 6, null, 1, null, false, null },
+                    { 5, 1, 6, null, 1, null, false, null },
+                    { 4, 1, 6, null, 1, null, false, null },
+                    { 3, 1, 6, null, 1, null, false, null },
+                    { 2, 1, 6, null, 1, null, false, null },
+                    { 8, 1, 6, null, 1, null, false, null },
+                    { 14, 1, 6, null, 1, null, false, null }
+                });
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DeleteData(
+                schema: "QuestionSet",
+                table: "ActivityQuestions",
+                keyColumns: new[] { "ActivityID", "QuestionID", "RequestFormVariantID" },
+                keyValues: new object[] { 1, 1, 6 });
+
+            migrationBuilder.DeleteData(
+                schema: "QuestionSet",
+                table: "ActivityQuestions",
+                keyColumns: new[] { "ActivityID", "QuestionID", "RequestFormVariantID" },
+                keyValues: new object[] { 2, 1, 6 });
+
+            migrationBuilder.DeleteData(
+                schema: "QuestionSet",
+                table: "ActivityQuestions",
+                keyColumns: new[] { "ActivityID", "QuestionID", "RequestFormVariantID" },
+                keyValues: new object[] { 3, 1, 6 });
+
+            migrationBuilder.DeleteData(
+                schema: "QuestionSet",
+                table: "ActivityQuestions",
+                keyColumns: new[] { "ActivityID", "QuestionID", "RequestFormVariantID" },
+                keyValues: new object[] { 4, 1, 6 });
+
+            migrationBuilder.DeleteData(
+                schema: "QuestionSet",
+                table: "ActivityQuestions",
+                keyColumns: new[] { "ActivityID", "QuestionID", "RequestFormVariantID" },
+                keyValues: new object[] { 5, 1, 6 });
+
+            migrationBuilder.DeleteData(
+                schema: "QuestionSet",
+                table: "ActivityQuestions",
+                keyColumns: new[] { "ActivityID", "QuestionID", "RequestFormVariantID" },
+                keyValues: new object[] { 6, 1, 6 });
+
+            migrationBuilder.DeleteData(
+                schema: "QuestionSet",
+                table: "ActivityQuestions",
+                keyColumns: new[] { "ActivityID", "QuestionID", "RequestFormVariantID" },
+                keyValues: new object[] { 7, 1, 6 });
+
+            migrationBuilder.DeleteData(
+                schema: "QuestionSet",
+                table: "ActivityQuestions",
+                keyColumns: new[] { "ActivityID", "QuestionID", "RequestFormVariantID" },
+                keyValues: new object[] { 8, 1, 6 });
+
+            migrationBuilder.DeleteData(
+                schema: "QuestionSet",
+                table: "ActivityQuestions",
+                keyColumns: new[] { "ActivityID", "QuestionID", "RequestFormVariantID" },
+                keyValues: new object[] { 9, 1, 6 });
+
+            migrationBuilder.DeleteData(
+                schema: "QuestionSet",
+                table: "ActivityQuestions",
+                keyColumns: new[] { "ActivityID", "QuestionID", "RequestFormVariantID" },
+                keyValues: new object[] { 10, 1, 6 });
+
+            migrationBuilder.DeleteData(
+                schema: "QuestionSet",
+                table: "ActivityQuestions",
+                keyColumns: new[] { "ActivityID", "QuestionID", "RequestFormVariantID" },
+                keyValues: new object[] { 11, 1, 6 });
+
+            migrationBuilder.DeleteData(
+                schema: "QuestionSet",
+                table: "ActivityQuestions",
+                keyColumns: new[] { "ActivityID", "QuestionID", "RequestFormVariantID" },
+                keyValues: new object[] { 12, 2, 6 });
+
+            migrationBuilder.DeleteData(
+                schema: "QuestionSet",
+                table: "ActivityQuestions",
+                keyColumns: new[] { "ActivityID", "QuestionID", "RequestFormVariantID" },
+                keyValues: new object[] { 12, 3, 6 });
+
+            migrationBuilder.DeleteData(
+                schema: "QuestionSet",
+                table: "ActivityQuestions",
+                keyColumns: new[] { "ActivityID", "QuestionID", "RequestFormVariantID" },
+                keyValues: new object[] { 12, 4, 6 });
+
+            migrationBuilder.DeleteData(
+                schema: "QuestionSet",
+                table: "ActivityQuestions",
+                keyColumns: new[] { "ActivityID", "QuestionID", "RequestFormVariantID" },
+                keyValues: new object[] { 12, 5, 6 });
+
+            migrationBuilder.DeleteData(
+                schema: "QuestionSet",
+                table: "ActivityQuestions",
+                keyColumns: new[] { "ActivityID", "QuestionID", "RequestFormVariantID" },
+                keyValues: new object[] { 13, 1, 6 });
+
+            migrationBuilder.DeleteData(
+                schema: "QuestionSet",
+                table: "ActivityQuestions",
+                keyColumns: new[] { "ActivityID", "QuestionID", "RequestFormVariantID" },
+                keyValues: new object[] { 14, 1, 6 });
+
+            migrationBuilder.DropColumn(
+                name: "Location",
+                schema: "QuestionSet",
+                table: "ActivityQuestions");
+
+            migrationBuilder.DropColumn(
+                name: "PlaceholderText",
+                schema: "QuestionSet",
+                table: "ActivityQuestions");
+
+            migrationBuilder.DropColumn(
+                name: "Subtext",
+                schema: "QuestionSet",
+                table: "ActivityQuestions");
+        }
+    }
+}
