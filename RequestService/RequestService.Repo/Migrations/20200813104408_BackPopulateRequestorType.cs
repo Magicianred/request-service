b064de52ca31ed4d409d2ef233a9bfe31fc5cedd
Jawwad Mukhtar <jawwad@factor-50.co.uk>
2020-08-13 11:53:10 +0100

    Back populate RequestorType Remove unused fields

---
+++ RequestService/RequestService.Repo/Migrations/20200813104408_BackPopulateRequestorType.cs
@@ -0,0 +1,21 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace RequestService.Repo.Migrations
+{
+    public partial class BackPopulateRequestorType : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.Sql(@"
+                UPDATE [Request].[Request] SET RequestorType = 2 WHERE RequestorType is null;
+            ");
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.Sql(@"
+                UPDATE [Request].[Request] SET RequestorType = 2 WHERE RequestorType is null;
+            ");
+        }
+    }
+}
