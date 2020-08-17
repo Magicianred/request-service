20c88088e2370502dd483fa153c39659b24b4571
Jawwad Mukhtar <jawwad@factor-50.co.uk>
2020-08-17 12:15:28 +0100

    add open job summaries

---
+++ RequestService/RequestService.Core/Domains/Entities/GetJobsByStatusesRequest.cs
@@ -0,0 +1,10 @@
+﻿using HelpMyStreet.Contracts.RequestService.Request;
+using MediatR;
+
+namespace RequestService.Core.Domains.Entities
+{
+    public class GetJobsByStatusesRequest : IRequest<GetJobsByStatusesResponse>
+    {
+        public JobStatusRequest JobStatuses { get; set; }
+    }
+}
