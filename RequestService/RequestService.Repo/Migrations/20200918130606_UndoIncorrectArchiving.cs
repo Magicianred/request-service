8ce0ac5d140d1f4fde5bc907d2a54b4a9fb03569
Jawwad Mukhtar <jawwad@factor-50.co.uk>
2020-09-18 15:36:53 +0100

    add unarchiving

---
+++ RequestService/RequestService.Repo/Migrations/20200918130606_UndoIncorrectArchiving.cs
@@ -0,0 +1,23 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace RequestService.Repo.Migrations
+{
+    public partial class UndoIncorrectArchiving : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.Sql(@"
+                            update Request.Request Set Archive=null
+                            from Request.Request r
+                            inner join Request.Job j on r.ID = j.RequestId
+                            where Archive = 1 and j.StatusID in (1,2)
+                        ");
+
+            //check if all jobs can be archived before archiving a request - TODO
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+        }
+    }
+}
