7af7c61553113a3a24ab63511aac7bfcec480dc3
Mike Matthews <mike@factor-50.co.uk>
2020-10-29 21:04:34 +0000

    GPS-216 "Anything else" text

--- RequestService/RequestService.Repo/Migrations/20201029210416_AnythingElseTextPart2.Designer.cs
+++ RequestService/RequestService.Repo/Migrations/20201029210416_AnythingElseTextPart2.Designer.cs
@@ -3,15 +3,17 @@ using System;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
 using Microsoft.EntityFrameworkCore.Metadata;
+using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 using RequestService.Repo;
 
 namespace RequestService.Repo.Migrations
 {
     [DbContext(typeof(ApplicationDbContext))]
-    partial class ApplicationDbContextModelSnapshot : ModelSnapshot
+    [Migration("20201029210416_AnythingElseTextPart2")]
+    partial class AnythingElseTextPart2
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder
